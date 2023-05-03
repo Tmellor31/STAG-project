87d3a5bfe354041b97310d7be70a3ff7e1a0fc53
Tom  Mellor <tommellor31@gmail.com>
2023-05-03 19:53:17 +0100

    Added some funny custom texts

--- src/main/java/edu/uob/GameServer.java
+++ src/main/java/edu/uob/GameServer.java
@@ -30,8 +30,8 @@ public final class GameServer {
     private static final char END_OF_TRANSMISSION = 4;
 
     public static void main(String[] args) throws IOException {
-        File entitiesFile = Paths.get("config" + File.separator + "extended-entities.dot").toAbsolutePath().toFile();
-        File actionsFile = Paths.get("config" + File.separator + "extended-actions.xml").toAbsolutePath().toFile();
+        File entitiesFile = Paths.get("config" + File.separator + "custom-entities.dot").toAbsolutePath().toFile();
+        File actionsFile = Paths.get("config" + File.separator + "custom-actions.xml").toAbsolutePath().toFile();
         GameServer server = new GameServer(entitiesFile, actionsFile);
         server.blockingListenOn(8888);
     }
