64da1b42467f3c1203870e702ee903ed7f4cdf73
Tom  Mellor <tommellor31@gmail.com>
2023-05-03 19:39:26 +0100

    Basic xml file works

---
+++ src/test/java/edu/uob/BasicTests.java
@@ -0,0 +1,38 @@
+package edu.uob;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BasicTests {
+
+    private GameServer server;
+
+    // Create a new server _before_ every @Test
+    @BeforeEach
+    void setup() {
+        File entitiesFile = Paths.get("config" + File.separator + "basic-entities.dot").toAbsolutePath().toFile();
+        File actionsFile = Paths.get("config" + File.separator + "basic-actions.xml").toAbsolutePath().toFile();
+        server = new GameServer(entitiesFile, actionsFile);
+    }
+
+    String sendCommandToServer(String command) {
+        // Try to send a command to the server - this call will timeout if it takes too long (in case the server enters an infinite loop)
+        return assertTimeoutPreemptively(Duration.ofMillis(1000), () -> {
+                    return server.handleCommand(command);
+                },
+                "Server took too long to respond (probably stuck in an infinite loop)");
+    }
+
+    @Test
+    void testGet() {
+        String response = sendCommandToServer("simon: get potion");
+        response = response.toLowerCase();
+        assertTrue(response.contains("you added the"), "Failed to pick up potion");
+    }
+}
